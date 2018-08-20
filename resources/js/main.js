37bcda6edc43e557813e21b7cbdc2b96cedc0571
IHSIEN <37260154+shirleyxx100@users.noreply.github.com>
2018-08-20 15:33:53 +0200

    Back up file

---
+++ resources/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  $('.btn').on('click', () => {
+    $('.pop-up').show();
+    $('.black').show();
+  });
+
+  $('.close').on('click', () => {
+    $('.pop-up').hide();
+    $('.black').hide();
+  });
+
+  $('.black').on('click', () => {
+    $('.pop-up').hide();
+    $('.black').hide();
+  });
+
+  $('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    adaptiveHeight: false
+  });
+
+  $('.carousel-six').slick({
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    adaptiveHeight: false
+  });
+});
